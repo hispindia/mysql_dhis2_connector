f5dbdd420f27e67907c21c396c5670071ed4efa0
Mithilesh Kumar Thakur <mithilesh.hisp@gmail.com>
2024-06-05 13:02:38 +0530

    revert from main branch

--- constants.py
+++
@@ -1,9 +0,0 @@
-# constants.py
-
-from datetime import datetime
-
-LOG_FILE_ENROLLMENT = datetime.now().strftime("%Y-%m-%d") + "_enrollment.txt"
-LOG_FILE_EVENT = datetime.now().strftime("%Y-%m-%d") + "_event.txt"
-
-
-
